4fd55d0cc2450ea8ef7097868529912d8ce65594
Thiago Silva <thiagof89.tf@gmail.com>
2022-02-12 13:58:22 -0300

    save data on fauna database

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,12 +1,36 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import { query as q } from 'faunadb';
+import NextAuth from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    }),
-    // ...add more providers here
-  ],
-})
+	secret: 'c48110bec3106bd48248bd04f97838ab',
+	providers: [
+		GithubProvider({
+			clientId: process.env.GITHUB_CLIENT_ID,
+			clientSecret: process.env.GITHUB_CLIENT_SECRET,
+		}),
+	],
+	callbacks: {
+		async signIn({ user, account, profile }) {
+			try {
+				const { email } = user;
+
+				await fauna.query(
+					q.If(
+						q.Not(
+							q.Exists(q.Match(q.Index('user_by_email'), q.Casefold(email))),
+						),
+						q.Create(q.Collection('users'), { data: email }),
+						q.Get(q.Match(q.Index('user_by_email'), q.Casefold(email))),
+					),
+				);
+
+				return true;
+			} catch (error) {
+				return false;
+			}
+		},
+	},
+});
